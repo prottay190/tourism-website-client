8d1192622e1e2726406c881f1bed7b8a55a775ad
unknown <aarnob2020@gmail.com>
2022-01-13 13:17:23 +0600

    update details page

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import './Details.css'
 
 const Details = () => {
@@ -14,13 +16,13 @@ const Details = () => {
     
     return (
         <div>
-           <h2>Service Id: {serviceId}</h2>
            <div className="details-Container">
-               <img src={service.img} alt="" />
-               <h2>name: {service.name}</h2>
-               <h5>Packages: {service.packages}</h5>
-               <h6>Price: {service.price}</h6>
+               <img  src={service.img} className='img-fluid center' alt='Responsive image'  />
+               <h2>Name: {service.name}</h2>
                <p>Description: {service.description}</p>
+               <h5>Packages: {service.packages}</h5>
+               <h6>Price: ${service.price}</h6>
+               <Link to="/shipping"><Button className='details-btn'>Order Now</Button> </Link>
            </div>
         </div>
     );
