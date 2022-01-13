94e8d7f29a335ce3835fcf2b89d6c8b861661300
unknown <aarnob2020@gmail.com>
2022-01-13 20:04:15 +0600

    updated

--- src/Pages/Firebase/firebase.config.js
+++ src/Pages/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCGjYrElhrmBPBdbj9I1CMkNH0iuviwGr8",
-    authDomain: "tourism-website-ab239.firebaseapp.com",
-    projectId: "tourism-website-ab239",
-    storageBucket: "tourism-website-ab239.appspot.com",
-    messagingSenderId: "543750651290",
-    appId: "1:543750651290:web:ede8db4f9e98494fa80e23"
-  };
+  apiKey: "AIzaSyCGjYrElhrmBPBdbj9I1CMkNH0iuviwGr8",
+  authDomain: "tourism-website-ab239.firebaseapp.com",
+  projectId: "tourism-website-ab239",
+  storageBucket: "tourism-website-ab239.appspot.com",
+  messagingSenderId: "543750651290",
+  appId: "1:543750651290:web:ede8db4f9e98494fa80e23"
+};
 
   export default firebaseConfig;
